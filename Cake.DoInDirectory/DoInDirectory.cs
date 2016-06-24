a1ece2fa8720c673014c6d07d5970b2aab79213f
Pedro Marques <phmarques@criticalsoftware.com>
2016-06-24 17:43:15 +0100

    initial commit

---
+++ Cake.DoInDirectory/DoInDirectory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace Cake.DoInDirectory
+{
+    public static class MyCakeExtension
+    {
+        [CakeMethodAlias]
+        public static void DoInDirectory(this ICakeContext context, DirectoryPath directory, Action task)
+        {
+            var original = context.Environment.WorkingDirectory;
+            try
+            {
+                context.Environment.WorkingDirectory = directory.MakeAbsolute(context.Environment);
+                task();
+            }
+            finally
+            {
+                context.Environment.WorkingDirectory = original;
+            }
+        }
+    }
+}
