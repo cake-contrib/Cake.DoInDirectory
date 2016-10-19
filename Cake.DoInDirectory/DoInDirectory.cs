170dc94a1c264f02f2e01cffb8c94fa226da3a20
Pedro Henrique Marques <phmarques@criticalsoftware.com>
2016-10-19 10:37:11 +0100

    added xml documentation. released v1

--- Cake.DoInDirectory/DoInDirectory.cs
+++ Cake.DoInDirectory/DoInDirectory.cs
@@ -5,8 +5,38 @@ using Cake.Core.IO;
 
 namespace Cake.DoInDirectory
 {
-    public static class MyCakeExtension
+    /// <summary>
+    /// Contains functionality for changing the current directory for an operation
+    /// </summary>
+    [CakeAliasCategory("DoInDirectory")]
+    public static class DoInDirectoryAliases
     {
+        /// <summary>
+        /// Changes the current working directory before an <see cref="Action"/> and returns to the previous one in the end
+        /// </summary>
+        /// <example>
+        /// <para>Use the #addin preprocessor directive</para>
+        /// <code>
+        /// <![CDATA[
+        /// #addin nuget:?package=Cake.DoInDirectory
+        /// ]]>
+        /// </code>
+        /// <para>Cake task:</para>
+        /// <code>
+        /// <![CDATA[
+        /// Task("SomeTask").Does(() =>
+        /// {
+        ///     DoInDirectory("./SomeDir", () =>
+        ///     {
+        ///         // Do awesome stuff
+        ///     });
+        /// });
+        /// ]]>
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="directory">The desired working directory for the action.</param>
+        /// <param name="task">The action to be executed.</param>
         [CakeMethodAlias]
         public static void DoInDirectory(this ICakeContext context, DirectoryPath directory, Action task)
         {
